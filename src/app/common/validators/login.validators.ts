78b7b314194dc6613913b98559b62aebff35e5a3
MedievalSerj <ladonya.s@gmail.com>
2017-09-14 19:31:40 +0300

    fUUUUCK

--- src/app/common/validators/login.validators.ts
+++
@@ -1,9 +0,0 @@
-
-import {AbstractControl, ValidationErrors} from '@angular/forms';
-
-export class LoginValidators {
-
-  static cannotContainSpecial(control: AbstractControl) : ValidationErrors | null {
-    return null;
-  }
-}
--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -1,30 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
+import {MyValidators} from '../common/validators/my.validators';
+
 
 @Component({
   selector: 'app-sign-up-form',
   templateUrl: './sign-up-form.component.html',
   styleUrls: ['./sign-up-form.component.css']
 })
-export class SignUpFormComponent implements OnInit {
+export class SignUpFormComponent {
+  form: FormGroup;
 
-  form = new FormGroup({
-    login: new FormControl('', [Validators.required,
-                                Validators.minLength(3)]),
-    first_name: new  FormControl('', [Validators.required]),
-    last_name: new FormControl(),
-    email: new FormControl(),
-    passwd: new FormControl(),
-    confirm_passwd: new FormControl()
-  });
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      login: ['', Validators.required,
+        Validators.minLength(3),
+        MyValidators.cannotContainSpecial,
+        MyValidators.maxLenthReached],
+      first_name: ['', Validators.required,
+        Validators.minLength(2),
+        MyValidators.onlyLetters],
+      last_name: ['',
+        Validators.required,
+        Validators.minLength(2),
+        MyValidators.onlyLetters
+      ],
+      email: ['',
+        MyValidators.myEmail,
+      ],
+      passwd: ['', MyValidators.myPassword],
+      confirm_passwd: ['', Validators.required],
+    }, {validator: MyValidators.confirmPassword});
+  }
 
   get login() {
     return this.form.get('login');
   }
 
-  constructor() { }
+  get first_name() {
+    return this.form.get('first_name');
+  }
 
-  ngOnInit() {
+  get last_name() {
+    return this.form.get('last_name');
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  get passwd() {
+    return this.form.get('passwd');
+  }
+
+  get confirm_passwd() {
+    return this.form.get('confirm_passwd');
   }
 
 }
