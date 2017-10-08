cb74911f0698d127ba7e33c02045016e6e74f14a
MedievalSerj <ladonya.s@gmail.com>
2017-10-08 19:01:40 +0300

    add sorting ang filtring for /

--- src/app/gallery-preview/gallery-preview.component.ts
+++ src/app/gallery-preview/gallery-preview.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {PreviewsService} from '../services/previews.service';
 import {CardPreviewComponent} from '../card-preview/card-preview.component';
+import {isUndefined} from 'util';
 
 
 @Component({
@@ -11,20 +12,148 @@ import {CardPreviewComponent} from '../card-preview/card-preview.component';
 export class GalleryPreviewComponent implements OnInit {
 
   public previews: any[] = [];
+  public previews_backup: any[] = [];
 
   constructor(private preview_service: PreviewsService) {
-  }
-
-  sort(param, order) {
-   console.log('sort by: ' + param);
-   console.log('order is: ' + order);
   }
 
   ngOnInit() {
     this.preview_service.readAll()
       .subscribe(response => {
-        console.log(response['search_results']);
         this.previews = response['search_results'];
+        this.previews.sort(this.comp_title_asc);
+        this.previews_backup = this.previews;
       });
+  }
+
+  sort(param, order) {
+    if (param === 'title') {
+      if (order)
+        this.previews.sort(this.comp_title_asc);
+      else
+        this.previews.sort(this.comp_title_dec);
+    } else if (param === 'genre') {
+      if (order)
+        this.previews.sort(this.comp_genre_asc);
+      else
+        this.previews.sort(this.comp_genre_dec);
+    } else if (param === 'rating') {
+      if (order)
+        this.previews.sort(this.comp_rating_asc);
+      else
+        this.previews.sort(this.comp_rating_dec);
+    } else if (param === 'year') {
+      if (order)
+        this.previews.sort(this.comp_year_asc);
+      else
+        this.previews.sort(this.comp_year_dec);
+    }
+  }
+
+  apply_filter(title,
+               genre,
+               rating_from,
+               rating_to,
+               year_from,
+               year_to) {
+    this.previews = this.previews_backup;
+    if (!isUndefined(title) && title !== '') {
+      this.previews = this.previews_backup.filter(item => item.title.indexOf(title) !== -1);
+    }
+    if (!isUndefined(genre) && genre !== '') {
+      this.previews = this.previews.filter(item => item.genre.indexOf(genre) !== -1);
+    }
+    if (!isUndefined(rating_from) && rating_from !== '') {
+      let n = parseInt(rating_from);
+      this.previews = this.previews.filter(item => item.rating >= n);
+    }
+    if (!isUndefined(rating_to) && rating_to !== '') {
+      let n = parseInt(rating_to);
+      this.previews = this.previews.filter(item => item.rating <= n);
+    }
+    if (!isUndefined(year_from) && year_from !== '') {
+      let n = parseInt(year_from);
+      this.previews = this.previews.filter(item => item.year >= n);
+    }
+    if (!isUndefined(year_to) && year_to !== '') {
+      let n = parseInt(year_to);
+      this.previews = this.previews.filter(item => item.year <= n);
+    }
+  }
+
+  reset_filters() {
+    this.previews = this.previews_backup;
+  }
+
+  comp_title_asc(param1, param2) {
+    if (param1.title < param2.title)
+      return -1;
+    else if (param1.title > param2.title)
+      return 1;
+    else
+      return 0;
+  }
+
+  comp_title_dec(param1, param2) {
+    if (param1.title > param2.title)
+      return -1;
+    else if (param1.title < param2.title)
+      return 1;
+    else
+      return 0;
+  }
+
+  comp_genre_asc(param1, param2) {
+    if (param1.genre < param2.genre)
+      return -1;
+    else if (param1.genre > param2.genre)
+      return 1;
+    else
+      return 0;
+  }
+
+  comp_genre_dec(param1, param2) {
+    if (param1.genre > param2.genre)
+      return -1;
+    else if (param1.genre < param2.genre)
+      return 1;
+    else
+      return 0;
+  }
+
+  comp_rating_asc(param1, param2) {
+    if (param1.rating < param2.rating)
+      return -1;
+    else if (param1.rating > param2.rating)
+      return 1;
+    else
+      return 0;
+  }
+
+  comp_rating_dec(param1, param2) {
+    if (param1.rating > param2.rating)
+      return -1;
+    else if (param1.rating < param2.rating)
+      return 1;
+    else
+      return 0;
+  }
+
+  comp_year_asc(param1, param2) {
+    if (param1.year < param2.year)
+      return -1;
+    else if (param1.year > param2.year)
+      return 1;
+    else
+      return 0;
+  }
+
+  comp_year_dec(param1, param2) {
+    if (param1.year > param2.year)
+      return -1;
+    else if (param1.year < param2.year)
+      return 1;
+    else
+      return 0;
   }
 }
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -9,18 +9,28 @@ import {GalleryPreviewComponent} from '../gallery-preview/gallery-preview.compon
 export class GalleryComponent implements OnInit {
 
   @ViewChild(GalleryPreviewComponent) galleryPreview;
+
+  title_filter: string;
+  genre_filter: string;
+  rating_filter_from: string;
+  rating_filter_to: string;
+  year_filter_from: string;
+  year_filter_to: string;
+
   title_order = true;
   genre_order = true;
   rating_order = true;
   year_order = true;
 
-  constructor() { }
-
-  orderby(param, order) {
-    this.galleryPreview.sort(param, order);
+  constructor() {
   }
 
   ngOnInit() {
   }
+
+  debugPrint() {
+
+  }
+
 
 }
