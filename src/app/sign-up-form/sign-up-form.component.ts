2e418ed68f9059a3070dd71e0e8067aca6f05594
MedievalSerj <ladonya.s@gmail.com>
2017-10-23 16:30:01 +0300

    before refactoring sign-in page

--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -42,7 +42,7 @@ export class SignUpFormComponent {
       ],
       email: ['',
         [MyValidators.myEmail],
-        // [this.emailOccupied.bind(this)]
+        [this.emailOccupied.bind(this)]
       ],
       passwd: ['', [MyValidators.myPassword]],
       confirm_passwd: ['', [Validators.required]],
